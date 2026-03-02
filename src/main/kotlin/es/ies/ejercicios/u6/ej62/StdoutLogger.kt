2c39fca92a57940123904b717d28a9134f75cee1
ifonlob <ifoncubiertal@iesrafaelalberti.es>
2026-03-02 17:48:03 +0100

    Prints de logs

--- src/main/kotlin/es/ies/ejercicios/u6/ej62/StdoutLogger.kt
+++ src/main/kotlin/es/ies/ejercicios/u6/ej62/StdoutLogger.kt
@@ -2,12 +2,12 @@ package es.ies.ejercicios.u6.ej62
 
 open class BaseLogger : Logger {
     override fun log(message: String) {
-        TODO("Implementa el log base (si procede)")
+        println(message)
     }
 }
 
 class StdoutLogger : BaseLogger() {
     final override fun log(message: String) {
-        TODO("Implementa el log final (bloquea sobrescritura con final override)")
+        println("LOG : $message")
     }
 }
