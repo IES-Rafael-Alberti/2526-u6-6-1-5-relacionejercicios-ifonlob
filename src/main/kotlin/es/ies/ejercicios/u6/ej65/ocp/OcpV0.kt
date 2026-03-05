245dc3e09fa2579c63456db3cf87d2e2c240ff70
ifonlob <ifoncubiertal@iesrafaelalberti.es>
2026-03-05 17:47:38 +0100

    Arreglado bugs de retorno

--- src/main/kotlin/es/ies/ejercicios/u6/ej65/ocp/OcpV0.kt
+++ src/main/kotlin/es/ies/ejercicios/u6/ej65/ocp/OcpV0.kt
@@ -3,30 +3,33 @@ package es.ies.ejercicios.u6.ej65.ocp
 import es.ies.ejercicios.u6.ej64.Resumible
 
 interface Generable{
-    fun generar(titulo: String, items: List<Resumible>)
+    fun generar(titulo: String, items: List<Resumible>) : String
 }
 
 class FormatoMarkdown : Generable{
     override fun generar(titulo: String, items: List<Resumible>) : String {
-        buildString {
+        val informe = buildString {
             appendLine("# $titulo")
             for (item in items) appendLine("- ${item.resumen()}")
         }
+        return informe
     }
 }
 class FormatoCsv : Generable{
     override fun generar(titulo: String, items: List<Resumible>) : String{
-        buildString {
+        val informe = buildString {
             appendLine("titulo,$titulo")
             appendLine("item")
             for (item in items) appendLine(item.resumen().replace(",", ";"))
         }
+        return informe
     }
 }
 
 class GeneradorInformeV1(val generable : Generable) {
-    fun generarInforme(titulo: String, items: List<Resumible>){
-        generable.generar(titulo, items)
+    fun generarInforme(titulo: String, items: List<Resumible>) : String{
+        val informe = generable.generar(titulo, items)
+        return informe
     }
 }
 
@@ -39,10 +42,19 @@ fun main() {
         },
         object : Resumible {
             override fun resumen(): String = "Elemento B"
-        },
+        }
     )
 
-    val generador = GeneradorInformeV0()
-    println(generador.generar(FormatoInformeV0.MARKDOWN, "Demo OCP", items))
+    println("=== Prueba Formato Markdown ===")
+    val formatoMarkdown = FormatoMarkdown()
+    val generadorMarkdown = GeneradorInformeV1(formatoMarkdown)
+    val informeMarkdown = generadorMarkdown.generarInforme("Prueba", items)
+    println(informeMarkdown)
+
+    println("\n=== Prueba Formato CSV ===")
+    val formatoCsv = FormatoCsv()
+    val generadorCsv = GeneradorInformeV1(formatoCsv)
+    val informeCsv = generadorCsv.generarInforme("Prueba", items)
+    println(informeCsv)
 }
 
