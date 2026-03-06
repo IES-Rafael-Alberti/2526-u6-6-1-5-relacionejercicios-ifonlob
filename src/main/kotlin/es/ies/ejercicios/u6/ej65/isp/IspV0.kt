66b538aef23d47359bf2f2079af5091cf9441f24
ifonlob <ifoncubiertal@iesrafaelalberti.es>
2026-03-06 10:39:11 +0100

    Cumplimiento de ISP

--- src/main/kotlin/es/ies/ejercicios/u6/ej65/isp/IspV0.kt
+++ src/main/kotlin/es/ies/ejercicios/u6/ej65/isp/IspV0.kt
@@ -1,18 +1,27 @@
+
 package es.ies.ejercicios.u6.ej65.isp
 
 import es.ies.ejercicios.u6.ej64.Persona
 
-/**
- * v0 (viola ISP): interfaz "gorda" que fuerza a implementar métodos que algunos clientes no necesitan.
- */
-interface RepositorioPersonasCompletoV0 {
+
+
+
+interface  Guardable{
     fun guardar(persona: Persona)
-    fun buscar(nombre: String): Persona?
-    fun exportarCsv(): String
-    fun borrarTodo()
 }
 
-class RepositorioMemoriaV0 : RepositorioPersonasCompletoV0 {
+interface Buscable{
+    fun buscar(nombre: String): Persona?
+}
+
+interface Exportable{
+    fun exportarCsv(): String
+}
+
+interface Borrable{
+    fun borrarTodo()
+}
+class RepositorioMemoriaV1 : Guardable, Buscable, Exportable, Borrable {
     private val map = mutableMapOf<String, Persona>()
 
     override fun guardar(persona: Persona) {
@@ -35,12 +44,17 @@ class RepositorioMemoriaV0 : RepositorioPersonasCompletoV0 {
 /**
  * Cliente que solo necesita buscar, pero depende de una interfaz con demasiadas cosas.
  */
-class BuscadorPersonasV0(private val repo: RepositorioPersonasCompletoV0) {
+class BuscadorPersonasV0(private val repo: Buscable) {
     fun buscar(nombre: String): Persona? = repo.buscar(nombre)
 }
 
+class ExportadorCSV(private val repo: Exportable) {
+    fun exportar(): String = repo.exportarCsv()
+}
+
+
 fun main() {
-    val repo = RepositorioMemoriaV0()
+    val repo = RepositorioMemoriaV1()
     repo.guardar(Persona("Ana", 20))
 
     val buscador = BuscadorPersonasV0(repo)
